cb07d4a138e65fadc2c9998df59e886f1d538049
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-02-09 14:12:57 +0400

    int Ok

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Delegate
 {
-
     public delegate void MyDelegate();  //first Delegate void
-    public delegate int Deleg();//second delegate int
+    public delegate int DelegateInt();//second delegate int
+    public delegate double DelegateDouble();
 
     class Program
     {
@@ -20,14 +20,21 @@ namespace Delegate
             d2();
             // Delay.
             //-----------------------------Second Delegate
-            Deleg d1 = Func1;
-            d1 += Func1;
-            Console.WriteLine(d1());
-            Func2();
-            d1 += Func2;
-            Console.WriteLine(d1());
+            DelegateInt d1 = Func1;
+                        d1 += Func1;
+                        Console.WriteLine(d1());
+                        Func2();
+                        d1 += Func2;
+                        Console.WriteLine(d1());
+            //-----------   Thrird Delegate
+            DelegateDouble B1=Print1;
+                           B1+= Print1;
+                           Console.WriteLine(B1());
+                           B1 += Print2;
+                           Print2();
+                           Console.WriteLine(B1());// Console.WriteLine(B1(Dont error without Scobes,-Why???));
 
-            Console.ReadKey();
+            Console.ReadLine();
         }
         static int Func1()
         {
@@ -47,6 +54,20 @@ namespace Delegate
         static void Method2()
         {
             Console.WriteLine("second method");
+        }
+        static double Print1()
+        {
+            int a = 7;
+            int b = 9;
+            return a * b;
+           
+        }
+        static double Print2()
+        {
+            int a = 7;
+            int b = 9;
+            return a / b;
+
         }
     }
 }
