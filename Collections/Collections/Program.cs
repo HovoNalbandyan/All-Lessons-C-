44acd3e5dcc2e42e9122155d89fbdf4117335430
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-02-10 18:03:54 +0400

    Dont understand any

--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -12,14 +12,12 @@ namespace InterIEnumerable
         static void Main()
         {
             UserCollection myCollection = new UserCollection();
-
-            // Используем foreach, для обращения к каждому объекту Element внутри массива myCollection. 
             foreach (Element element in myCollection)
             {
                 Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
             }
 
-            //myCollection.Reset(); // Убрать комментарий для проверки.
+            //myCollection.Reset(); //
 
             Console.Write(new string('-', 29) + "\n");
 
@@ -29,30 +27,20 @@ namespace InterIEnumerable
                 Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
             }
 
-            Console.Write(new string('-', 29) + "\n");
-
- 
+            Console.Write(new string('-', 29) + "\n"); 
             // ----------------------------------------------------------------------------------------------------------------------
             // Так работает foreach.
-
             UserCollection myElementsCollection = new UserCollection();
-
             // foreach - приводит коллекцию к интерфейсному типу IEnumerable.
             IEnumerable enumerable = myElementsCollection as IEnumerable;
-
             // foreach - приводит коллекцию к интерфейсному типу вызывая метод - GetEnumerator().            
             IEnumerator enumerator = enumerable.GetEnumerator();
-
-
             while (enumerator.MoveNext()) // Перемещаем курсор на 1 шаг вперед (с -1 на 0) и т.д.
             {
                 Element element = enumerator.Current as Element;
-
                 Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
             }
-
-            enumerator.Reset();
-
+                        enumerator.Reset();
             // Задержка.
             Console.ReadKey();
         }
