4e82cefe4271255cd54cb9028b3a4827e625361a
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-03-02 15:36:42 -0800

    Delegate Questions

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -6,68 +6,63 @@ using System.Threading.Tasks;
 
 namespace Delegate
 {
-    public delegate void MyDelegate();  //first Delegate void
+    public delegate void DelegateVoid();  //first Delegate void
     public delegate int DelegateInt();//second delegate int
     public delegate double DelegateDouble();
 
     class Program
 
     {
-        static int Func1()
-        {
-            return 1;
-        }
-
-        static int Func2()
-        {
-            return 2;
-        }
-
         static void Method1()
         {
             Console.WriteLine("First Method");
         }
-
         static void Method2()
         {
             Console.WriteLine("second method");
         }
+        static int Func1()
+        {
+            int f = 2;
+            int y = 1;
+            return f * y;
+        }
+        static int Func2()
+        {
+            return 22;
+        }        
         static double Print1()
         {
-            int a = 7;
-            int b = 9;
+            int a = 1;
+            int b = 3;
             return a * b;
-
         }
         static double Print2()
         {
-            int a = 7;
-            int b = 9;
+            int a = 9;
+            int b = 3;
             return a / b;
-
         }
         static void Main()
         { //----------------------------First Delegate
-            MyDelegate d2;
-            d2 = Method1;
-            d2 += Method2;
-            d2();
+            DelegateVoid d1;
+            d1 = Method1;
+            d1 += Method2;
+            d1();           
             // Delay.
             //-----------------------------Second Delegate
-            DelegateInt d1 = Func1;
-            d1 += Func1;
-            Console.WriteLine(d1());
-            Func2();
-            d1 += Func2;
-            Console.WriteLine(d1());
-            //-----------   Thrird Delegate
-            DelegateDouble B1 = Print1;
-            B1 += Print1;
+            DelegateInt d2;
+            d2 = Func1;     
+            //Console.WriteLine(d2());
+            d2 += Func2;
+            d2();
+            //Console.WriteLine(d2());            
+            //-----------Thrird Delegate
+            DelegateDouble B1 ;    
+            B1 = Print1;
             Console.WriteLine(B1());
             B1 += Print2;
-            Print2();
             Console.WriteLine(B1());// Console.WriteLine(B1(Dont error without Scobes,-Why???));
-
             Console.ReadLine();        
             
 
